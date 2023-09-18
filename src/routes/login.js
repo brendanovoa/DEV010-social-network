07c70f760282e952f000299fa521cd8232b7a66f
JazminRuiz98 <aida.jazmin.ruiz.orbe@gmail.com>
2023-09-18 13:45:17 -0600

    inicio de sesion funcionando

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,6 @@
+import { getRedirectResult } from 'firebase/auth';
 import {
-  GoogleAuthProvider, signInWithRedirect,
-} from 'firebase/auth';
-import {
-  signIn, auth, resetEmail,signinGoogle,
+  signIn, auth, resetEmail, signinGoogle, pruebaGoogle,
 } from '../firebase/firebaseConfig.js';
 
 function login(navigateTo) {
@@ -17,7 +15,7 @@ function login(navigateTo) {
   const linkRegister = document.createElement('span');
   const linkResetEmail = document.createElement('span');
   const buttonLogInWithGoogle = document.createElement('button');
-  buttonLogInWithGoogle.setAttribute('type', 'button');
+  buttonLogInWithGoogle.setAttribute('type', 'submit');
 
   inputEmail.placeholder = 'Correo electrónico';
   inputPass.placeholder = 'Contraseña';
@@ -50,12 +48,11 @@ function login(navigateTo) {
   });
 
   buttonLogInWithGoogle.textContent = 'ENTRA CON GOOGLE';
-  buttonLogInWithGoogle.addEventListener('click', (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const provider = new GoogleAuthProvider();
-    signinGoogle(provider);
-    console.log('funciono');
-    navigateTo('/feed');
+    pruebaGoogle().then((res) => {
+      navigateTo('/feed');
+    });
   });
 
   textRegister.textContent = 'Si aún no tienes cuenta regístrate ';
